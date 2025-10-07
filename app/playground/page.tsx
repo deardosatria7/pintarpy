927e42e75470c9ca13e2d9a849f119908610d9d5
Deardo Satria R. S <deardosatria1@gmail.com>
2025-10-07 09:28:53 +0700

    remove auth from playgrund

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,11 +1,6 @@
 import PlaygroundComponent from "@/components/playground-component";
-import {auth} from "@/auth";
-import {redirect} from "next/navigation";
 
 export default async function PlaygroundPage() {
-  const session = await auth();
-  if (!session?.user) return redirect("/login");
-
   return (
     <>
       <PlaygroundComponent />
